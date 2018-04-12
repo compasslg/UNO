b443b66920eb5830fc165bfa0d65aa8c50c3b2e7
Hanwei <lihanwei4c@gmail.com>
2018-04-12 12:29:05 -0700

    Add files via upload

---
+++ UNO/UNO/UNO.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "card.h"
+//== The main function ===========================================================
+int main(void) {
+	int numOfPlayers;
+	return 0;
+}
---
+++ UNO/UNO/card.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#define RED '♥'
+#define YELLOW '♦'
+#define GREEN '♣'
+#define BLUE '♠'
+//== A node type that represent a card ===========================================
+typedef struct card_s {
+	char suit;
+	int value;
+	char action[15];
+	struct card_s *pt;
+}card;
