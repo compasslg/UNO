7817d36b8bf9ae07700272e5ad56f669568e033a
ChElTePiN <antoniog20@email.arizona.edu>
2018-04-16 16:20:30 -0700

    Yes

---
+++ UNO/Final_Project/Final_Project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Final_Project
+//
+//  Created by Uriel Gonzalez on 4/16/18.
+//  Copyright © 2018 Antonio Gonzalez. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
